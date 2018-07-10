5fbcb88ae6cbc1a70bd95fe7718145a1be819403
Alexandr Sergeev <aserg@softserveinc.com>
2018-07-10 15:37:08 +0300

    7

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM node:alpine
-
-
 RUN apk add --no-cache vim mc
 COPY run.sh .
 COPY app app
+COPY database database
 RUN cd /app && npm i && cd ..
 RUN chmod +x run.sh
 
--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 echo "starting..."
-while true; do
-  sleep 0.1 # wait for 1/10 of the second before check again
-done
-#node /app/index.js
+#while true; do
+#  sleep 0.1 # wait for 1/10 of the second before check again
+#done
+if [ ! /database/rules.json ]; then
+  echo "file not found"
+  touch /database/rules.json
+fi
+node /app/index.js
